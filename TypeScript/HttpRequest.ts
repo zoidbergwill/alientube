230bf2941b3b3476bfe0ed593b14f724a1bc2ca6
William Stewart <zoidbergwill@gmail.com>
2019-04-09 14:07:15 +0200

    WIP

--- TypeScript/HttpRequest.ts
+++ TypeScript/HttpRequest.ts
@@ -16,14 +16,14 @@ module AlienTube {
         private static acceptableResponseTypes = [200, 201, 202, 301, 302, 303, 0];
 
         constructor(url: string, type: RequestType, callback: any, postData?: any, errorHandler?: any) {
-             if (Utilities.getCurrentBrowser() === Browser.SAFARI && safari.self.addEventListener) {
+            if (Utilities.getCurrentBrowser() === Browser.SAFARI && safari.self.addEventListener) {
                 /* Generate a unique identifier to identify our request and response through Safari's message system. */
                 let uuid = HttpRequest.generateUUID();
-                
+
                 /* Message the global page to have it perform a web request for us. */
                 let listener = safari.self.addEventListener('message', function listenerFunction(event) {
                     if (event.message.uuid !== uuid) return;
-                    
+
                     if (event.message.data && callback) {
                         callback(event.message.data);
                     } else if (event.message.error && errorHandler) {
@@ -31,14 +31,52 @@ module AlienTube {
                     }
                     safari.self.removeEventListener('message', listenerFunction, false);
                 }, false);
-    	       
+
                 safari.self.tab.dispatchMessage("XHR", {
                     'url': url,
                     'uuid': uuid,
                     'requestType': type,
                     'postData': postData
                 });
-                
+
+            } else if (Utilities.getCurrentBrowser() === Browser.CHROME && safari.self.addEventListener) {
+                chrome.runtime.onMessage.addListener(
+                    function (request, sender, sendResponse) {
+                        let xhr = new XMLHttpRequest();
+                        xhr.open(RequestType[type], url, true);
+                        xhr.withCredentials = true;
+                        if (type === RequestType.POST) {
+                            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                        }
+
+                        xhr.onerror = function (e) {
+                            if (errorHandler) errorHandler(xhr.status);
+                        }.bind(this);
+
+                        xhr.onload = function () {
+                            if (HttpRequest.acceptableResponseTypes.indexOf(xhr.status) !== -1) {
+                                /* This is an acceptable response, we can now call the callback and end successfuly. */
+                                if (callback) {
+                                    callback(xhr.responseText);
+                                }
+                            } else {
+                                /* There was an error */
+                                if (errorHandler) errorHandler(xhr.status);
+                            }
+                        }.bind(this);
+
+                        /* Convert the post data array to a query string. */
+                        if (type === RequestType.POST) {
+                            let query = [];
+                            for (let key in postData) {
+                                query.push(encodeURIComponent(key) + '=' + encodeURIComponent(postData[key]));
+                            }
+                            xhr.send(query.join('&'));
+                        } else {
+                            xhr.send();
+                        }
+                    }
+                );
             } else {
                 let xhr = new XMLHttpRequest();
                 xhr.open(RequestType[type], url, true);
@@ -50,7 +88,7 @@ module AlienTube {
                 xhr.onerror = function (e) {
                     if (errorHandler) errorHandler(xhr.status);
                 }.bind(this);
-                
+
                 xhr.onload = function () {
                     if (HttpRequest.acceptableResponseTypes.indexOf(xhr.status) !== -1) {
                         /* This is an acceptable response, we can now call the callback and end successfuly. */
@@ -62,7 +100,7 @@ module AlienTube {
                         if (errorHandler) errorHandler(xhr.status);
                     }
                 }.bind(this);
-                
+
                 /* Convert the post data array to a query string. */
                 if (type === RequestType.POST) {
                     let query = [];
@@ -84,7 +122,7 @@ module AlienTube {
         * @private
         */
         private static generateUUID(): string {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
                 let r = Math.random() * 16 | 0,
                     v = c === 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
